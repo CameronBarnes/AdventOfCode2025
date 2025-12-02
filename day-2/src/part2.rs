41e285a1bca2886fded71a6c1835a132f843805a
Cameron Barnes <cameron_barnes@outlook.com>
2025-12-01 21:55:50 -0800

    completed day 2 part 2

--- day-2/src/part2.rs
+++ day-2/src/part2.rs
@@ -3,20 +3,26 @@ use itertools::Itertools;
 fn check_valid_id(id: u64) -> bool {
     let str = id.to_string();
     let content = str.as_bytes();
-    if content.len().is_multiple_of(2) {
-        let max = content.len() / 2;
-        (1..=max).any(|size| {
-            let mut chunks = content.chunks_exact(size);
-            chunks.remainder().is_empty() && chunks.all_equal()
-        })
-    } else {
-        false
-    }
+    let max = content.len() / 2;
+    (1..=max).any(|size| {
+        let mut chunks = content.chunks_exact(size);
+        chunks.remainder().is_empty() && chunks.all_equal()
+    })
 }
 
+#[allow(clippy::missing_panics_doc)]
 #[must_use]
-pub fn process(_input: &str) -> String {
-    todo!("day-2 - part 2")
+pub fn process(input: &str) -> String {
+    input
+        .trim_end()
+        .split(',')
+        .flat_map(|range| {
+            let (start, end) = range.split_once('-').unwrap();
+            start.parse::<u64>().unwrap()..=end.parse::<u64>().unwrap()
+        })
+        .filter(|num| check_valid_id(*num))
+        .sum::<u64>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -24,9 +30,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_example_input() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!("4174379265", process(input));
+    }
+
+    #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = include_str!("../input.txt");
+        assert_eq!("45814076230", process(input));
     }
 }
